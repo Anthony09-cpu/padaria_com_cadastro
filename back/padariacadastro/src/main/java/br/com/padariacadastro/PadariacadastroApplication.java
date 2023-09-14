22daa8baac58ebb02bbfd3b513e9591a1180d3c8
Anthony09-cpu <alaide.barbosa191@gmail.com>
2023-09-14 14:28:56 -0300

    14/09

---
+++ back/padariacadastro/src/main/java/br/com/padariacadastro/PadariacadastroApplication.java
@@ -0,0 +1,13 @@
+package br.com.padariacadastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadariacadastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadariacadastroApplication.class, args);
+	}
+
+}
---
+++ back/padariacadastro/src/test/java/br/com/padariacadastro/PadariacadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.padariacadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadariacadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
